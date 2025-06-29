896a383719fce9f20ed5842f7dfeb91179c8ac54
Golovko, Yaroslav <golovko085@gmail.com>
2025-06-29 18:05:00 +0700

    develop generateRandomElements

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 const (
@@ -10,31 +12,53 @@ const (
 )
 
 // generateRandomElements generates random elements.
-func generateRandomElements(size int) []int {
+func generateRandomElements(size int) ([]int, error) {
 	// ваш код здесь
+	if size < 0 {
+		return []int{}, fmt.Errorf("size smaller than 0")
+	}
+
+	generator := rand.New(rand.NewSource(time.Now().Unix()))
+	randomElements := make([]int, size)
+	for i := 0; i < size; i++ {
+		randomElements[i] = generator.Int()
+	}
+
+	return randomElements, nil
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
 	// ваш код здесь
+	fmt.Println(data[0])
+
+	return 0
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
 	// ваш код здесь
+	fmt.Println(data[0])
+
+	return 0
 }
 
 func main() {
+	max, elapsed := 0, 0
+
 	fmt.Printf("Генерируем %d целых чисел", SIZE)
 	// ваш код здесь
+	numbers, _ := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
 	// ваш код здесь
+	maximum(numbers)
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
 	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
 	// ваш код здесь
+	maxChunks(numbers)
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,75 @@
 package main
 
+import (
+	"math"
+	"testing"
+)
+
 // Пишите тесты в этом файле
+
+func TestGenerateRandomElements(t *testing.T) {
+	t.Parallel()
+	t.Run("zero size", func(t *testing.T) {
+		sliceLen := 0
+		slice, err := generateRandomElements(sliceLen)
+
+		if len(slice) != sliceLen {
+			t.Errorf("Expected 0 length of slice but got %d\n", len(slice))
+		}
+
+		if err != nil {
+			t.Errorf("Expected nil in error but got %s\n", err.Error())
+		}
+	})
+
+	t.Run("minus size", func(t *testing.T) {
+		slice, err := generateRandomElements(-10)
+
+		if err == nil {
+			t.Errorf("Expected error but got nil\n")
+		}
+
+		if len(slice) != 0 {
+			t.Errorf("Expected 0 length of slice but got %d\n", len(slice))
+		}
+	})
+
+	t.Run("normal size", func(t *testing.T) {
+		sliceLen := 10
+		slice, err := generateRandomElements(sliceLen)
+
+		if err != nil {
+			t.Errorf("Expected nil in error but got %s\n", err.Error())
+		}
+
+		if len(slice) != sliceLen {
+			t.Errorf("Expected %d length of slice but got %d\n", sliceLen, len(slice))
+		}
+
+		// Didn't invent better way to check slice filling
+		if slice[0] == slice[1] {
+			t.Errorf("Expected first and second elements not equil. But got %d == %d\n."+
+				"Could be a wrong result, restart test and check.\n", slice[0], slice[1])
+		}
+	})
+
+	// Max size is int16
+	t.Run("edge size", func(t *testing.T) {
+		sliceLen := math.MaxInt16
+		slice, err := generateRandomElements(sliceLen)
+
+		if err != nil {
+			t.Errorf("Expected nil in error but got %s\n", err.Error())
+		}
+
+		if len(slice) != sliceLen {
+			t.Errorf("Expected %d length of slice but got %d\n", sliceLen, len(slice))
+		}
+
+		// Didn't invent better way to check slice filling
+		if slice[0] == slice[1] {
+			t.Errorf("Expected first and second elements not equil. But got %d == %d\n."+
+				"Could be a wrong result, restart test and check.\n", slice[0], slice[1])
+		}
+	})
+}
